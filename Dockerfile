15783df0f4fa32ee80d6c0301ef1e7e71e828f56
Antoine Deschênes <antoine@antoinedeschenes.com>
2021-06-08 16:23:53 -0400

    mmproxy test

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
+FROM golang:1.16 AS builder
+RUN CGO_ENABLED=0 go get github.com/path-network/go-mmproxy
+
 FROM atmoz/sftp:debian
-
-RUN apt update ; apt full-upgrade -y ; apt clean
-
+COPY --from=builder /go/bin/go-mmproxy /usr/local/bin/go-mmproxy
+RUN apt update ; apt full-upgrade -y ; apt install iptables -y ; apt clean
 COPY ./override.sh /
 
 ENTRYPOINT ["/override.sh"]
--- override.sh
+++ override.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+
+set -e
+
+ip rule add from 127.0.0.1/8 iif lo table 123
+ip route add local 0.0.0.0/0 dev lo table 123
+
+mmproxy_listen="-4 127.0.0.1:22"
+
+if [[ "$(sysctl -en net.ipv6.conf.lo.disable_ipv6)" == "0" ]]; then
+  ip -6 rule add from ::1/128 iif lo table 123
+  ip -6 route add local ::/0 dev lo table 123
+  mmproxy_listen="${mmproxy_listen} -6 [::1]:22"
+fi
+
+echo "0.0.0.0/0" > /tmp/go-mmproxy
+
+go-mmproxy -l 0.0.0.0:2222 ${mmproxy_listen} -allowed-subnets /tmp/go-mmproxy &
+
 if [ ! -z "${ssh_host_ed25519_key}" ]
 then
   echo "${ssh_host_ed25519_key}" > /etc/ssh/ssh_host_ed25519_key && chmod 600 /etc/ssh/ssh_host_ed25519_key
