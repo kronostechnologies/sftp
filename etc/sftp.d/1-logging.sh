b52328dce4da4ac248e5eb0323f5bbabd4ed06ce
Antoine Deschênes <antoine@antoinedeschenes.com>
2021-06-10 15:55:37 -0400

    log cleanup

--- etc/sftp.d/1-logging.sh
+++ etc/sftp.d/1-logging.sh
@@ -19,3 +19,4 @@ echo "LogLevel ${log_level:-INFO}" >> /etc/ssh/sshd_config
 sed -i "s~ForceCommand internal-sftp~ForceCommand internal-sftp -l ${log_level:-INFO}~g" /etc/ssh/sshd_config
 
 /opt/td-agent-bit/bin/td-agent-bit -c /etc/td-agent-bit/td-agent-bit.conf &
+sleep 2
